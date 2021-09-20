55fa18db0873781385f4613e09a374c9105c1e3b
Seb Ringrose <sebringrose@gmail.com>
2021-09-20 17:17:39 +0100

    flow creation working

--- js/flows.js
+++ js/flows.js
@@ -5,7 +5,7 @@ import {
 import FlowManager from 'https://cdn.skypack.dev/flowed';
 
 // take elements state from react-flow, read to create resolvers and FlowManager tasks JSON
-export const onConnectUpdateFlows = (selectedElement, elements, setElements, flows, setFlows) => {
+export const onConnectUpdateFlows = (selectedElement, elements, flows, setFlows) => {
     const components = [];
     const recursivelyPushComponents = (element, sorter=0) => {
         const el = element;
@@ -40,13 +40,11 @@ export const onConnectUpdateFlows = (selectedElement, elements, setElements, flo
     // YAYYY this works!
     console.log(components);
 
-    // do things with components to create tasks obj, element IDs array & resolvers obj
-    // push resultant flow into flows array state var/update if flow already exists
-    // update elements in flow with flow id
+    // create tasks obj, element IDs array & resolvers obj for flow
+    // push result into flows array state var/update if flow already exists
     const elementIDs = [];
     const tasks = {};
     const elWithFlowID = components.find(cmpnt => cmpnt.el.data.flowID);
-    console.log(elWithFlowID);
     const flowID = elWithFlowID && elWithFlowID.data && elWithFlowID.data.flowID
         ? elWithFlowID.data.flowID
         : `flow-${flows.length}`;
@@ -56,16 +54,6 @@ export const onConnectUpdateFlows = (selectedElement, elements, setElements, flo
         console.log(flowID);
         cmpnt.el.data.flowID = flowID;
         tasks[String.fromCharCode(65+i)] = cmpnt.el;
-        
-        console.log(cmpnt);
-
-        // add flowID to elements in state for rendering in UI
-        setElements(els => 
-            els.map(el => {
-                if (el.id === cmpnt.el.id) el.data = cmpnt.el.data;
-                return el;
-            })
-        )
     });
 
     setFlows(flows => {
@@ -74,9 +62,8 @@ export const onConnectUpdateFlows = (selectedElement, elements, setElements, flo
         if (index > -1) {
             flows[i] = flowData;
         } else flows.push(flowData);
-        console.log(flows);
-        return flows;
-    })
+        return [...flows];
+    });
 }
 
 // var f = new Function(function.inputs, function.body);
